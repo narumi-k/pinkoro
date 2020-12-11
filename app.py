5bd08011772c9b84dc601f536e572b9c92436738
shin kawa <xxxxtfoaxxxx@yahoo.co.jp>
2020-12-11 19:45:05 +0900

    202012111443

--- app.py
+++ app.py
@@ -6,74 +6,61 @@ app = Flask(__name__)
 # secretkeyの設定（sessionを使えるようにする）
 app.secret_key = "sunabaco"
 
-@app.route("/top")
+
+@app.route("/")
 def top():
     return render_template("top.html")
+
+
+@app.route("/bbs")
+def bbs():
+    return render_template("bbs.html")
+
 
 @app.route("/taiyou")
 def taiyou():
     return render_template("taiyou.html")
 
+
 @app.route("/ootago")
 def ootago():
     return render_template("ootago.html")
+
 
 @app.route("/kongou")
 def kongou():
     return render_template("kongou.html")
 
+
 @app.route("/matsutaka")
 def matsutaka():
     return render_template("matsutaka.html")
 
-@app.route("/regist",methods=["GET"])
+
+@app.route("/regist", methods=["GET"])
 def regist_get():
     if "user_id" in session:
         return render_template("/top")
     else:
         return render_template("regist.html")
 
-@app.route("/regist",methods=["POST"])
+
+@app.route("/regist", methods=["POST"])
 def regist_post():
     if "user_id" in session:
         return redirect("/top")
-    
+
     else:
         name = request.form.get("name")
         password = request.form.get("password")
 
         conn = sqlite3.connect("pinkoro.db")
         c = conn.cursor()
-        c.execute("insert into users values(null,?,?)",(name,password))
+        c.execute("insert into users values(null,?,?)", (name, password))
         conn.commit()
         c.close()
 
         return redirect("/top")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == "__main__":
