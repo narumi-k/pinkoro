daa3813c88994eea3534f013de39e1e100582af8
narumikawata <kawata.narumi@gmail.com>
2020-12-12 15:45:28 +0900

    app.py changes

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 app.secret_key = "sunabaco"
 
 
-
-
-
 @app.route("/")
 def top():
     return render_template("top.html")
@@ -29,97 +26,8 @@ def intro():
 
 
 
-@app.route("/taiyou")
-def taiyou():
-    return render_template("taiyou.html")
 
 
-@app.route("/ootago")
-def ootago():
-    return render_template("ootago.html")
-
-
-@app.route("/kongou")
-def kongou():
-    return render_template("kongou.html")
-
-
-@app.route("/matsutaka")
-def matsutaka():
-    return render_template("matsutaka.html")
-
-
-@app.route("/regist", methods=["GET"])
-def regist_get():
-    if "user_id" in session:
-        return render_template("/top")
-    else:
-        return render_template("regist.html")
-
-
-@app.route("/regist", methods=["POST"])
-def regist_post():
-    if "user_id" in session:
-
-        return redirect("/top")
-
-
-        return redirect("/")
-    
-
-    else:
-        name = request.form.get("name")
-        password = request.form.get("password")
-
-        conn = sqlite3.connect("pinkoro.db")
-        c = conn.cursor()
-        c.execute("insert into users values(null,?,?)", (name, password))
-        conn.commit()
-        c.close()
-
-        return redirect("/")
-
-@app.route("/",methods=["POST"])
-def login_post():
-    name = request.form.get("name")
-    password = request.form.get("password")
-
-
-    conn = sqlite3.connect("pinkoro.db")
-    c = conn.cursor()
-    c.execute("select id from users where name = ? and password = ?",(name,password))
-    user_id = c.fetchone()
-    c.close()
-
-    if user_id is None:
-        return render_template("regist.html")
-    else:
-        session["user_id"] = user_id[0]
-        return redirect("/")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-@app.route("/logout")
-def logout():
-    session.pop("user_id",None)
-    return redirect("/")
 
 
 @app.errorhandler(404)
