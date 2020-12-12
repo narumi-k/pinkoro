ee203382ade8adc1fefbac78e710a54bdc0ed11d
narumikawata <kawata.narumi@gmail.com>
2020-12-12 17:03:51 +0900

    bbs add route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3,datetime
 # flaskからFlaskなどなどをインポート(*を使うと全部インポートできるようになる)
 from flask import *
 # appという名前でFlaskアプリを作る
@@ -11,10 +11,31 @@ app.secret_key = "sunabaco"
 def top():
     return render_template("top.html")
 
-
 @app.route("/bbs")
 def bbs():
-    return render_template("bbs.html")
+    conn = sqlite3.connect("pinkoro.db")
+    c = conn.cursor()
+    c.execute("select name,comment,image,datetime from bbs")
+    comment_list = []
+    for row in c.fetchall():
+        comment_list.append({"name": row[0], "comment": row[1], "image":row[2], "time":row[3]})
+    c.close()
+    return render_template('bbs.html' , comment_list = comment_list)
+
+
+@app.route("/add",methods=["POST"])
+def add():
+    time = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    name = request.form.get("name")
+    comment = request.form.get("comment")
+
+    conn = sqlite3.connect("pinkoro.db")
+    c = conn.cursor()
+    c.execute("insert into bbs values(null,?,?,?,?)",(name,comment,image,time))
+    conn.commit()
+    conn.close()
+
+    return redirect("/bbs") 
 
 @app.route("/course")
 def course():
