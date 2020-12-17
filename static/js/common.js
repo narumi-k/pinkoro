73fa691b12cc2aef520718cdea987688c9e4bb5e
narumikawata <kawata.narumi@gmail.com>
2020-12-17 17:52:11 +0900

    omikuji comment

--- static/js/common.js
+++ static/js/common.js
@@ -13,6 +13,25 @@ $(".omikuji_btn").on("click",function(){
     var random_img = omikuji[num];
 
     $('.omikuji_random').attr('src', random_img);
+
+    var omikuji_comment = [
+        '<p>「おさんぽ中に素敵な風景に出会えそう！」</p>',
+        '<p>「おさんぽで姿勢が良くなる予感！」</p>',
+        '<p>「おさんぽすると運気上昇！」</p>',
+        '<p>「おさんぽ中に思い出せなかったことが思い出せるかも！」</p>',
+        '<p>「おさんぽすると美味しいご飯に出会えるかも！」</p>',
+        '<p>「おさんぽすると骨密度UP！」</p>',
+        '<p>「おさんぽ中に懐かしの友と再会するかも！」</p>',
+        '<p>「おさんぽしたら思わぬ臨時収入があるかも！」</p>',
+        '<p>「おさんぽでビタミンDを作り出そう！」</p>',
+        '<p>「おさんぽで真っ直ぐ家に帰れたあなたは、まだ大丈夫！」</p>',
+    ];
+
+    var num = Math.floor(Math.random() * omikuji_comment.length);
+    var random_comment = omikuji_comment[num];
+
+    $(".omikuji_comment").html(random_comment);
+
 });
 
 });
